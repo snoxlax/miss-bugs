6f28bffb24f34c2440379bd9593fc596e5315b97
Vlad Grey <Vlad9rey@gmail.com>
2025-11-25 17:08:27 +0200

    Fix

--- frontend/src/services/http-client.js
+++ frontend/src/services/http-client.js
@@ -10,7 +10,7 @@ const getBaseURL = () => {
 
   // For same-origin (backend serves frontend), use relative path
   // Empty string means same origin - works on localhost and Render
-  return 'http://localhost:3030';
+  return '';
 };
 
 const client = axios.create({
